d5ebf8fdd2697d8aadb964c423a5c37d52178430
R-Ross414 <42787615+R-Ross414@users.noreply.github.com>
2018-09-13 23:12:12 -0700

    Update Assignment1.h

--- Assignment1.h
+++ Assignment1.h
@@ -9,7 +9,7 @@ int calc_frequency(string DNA_str, char base);
 //this function calculates mean
 double calc_mean(int sum, int total_str);
 //this function calculates variance
-double calc_vari(int mean, int nuc);
+double calc_vari(int mean, int num_samples);
 //this function calculates standard deviation
 double calc_stdDev(double mean, int total);
 //this function calculates probability
